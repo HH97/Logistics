42d0bb45f21774a9672f339a2d8cf8fba8d0830e
William Smith <weiliang84@yeah.net>
2017-12-23 22:33:30 +0800

    try

--- Log_web/static/form.js
+++ Log_web/static/form.js
@@ -1,87 +1,137 @@
- $(document).ready(function() {
-	validateSignIn()
+$(document).ready(function() {
+  //alert("!!!")
+	 validateform()
+
+  $.ajax('/getProvince', {dataType:'json'}).done(function(data){
+    var ProvinceList = $("#ProvinceList")
+    for (var i = 0; i < data.province.length; i++) {
+      ProvinceList.append("<a class='dropdown-item btn btn-primary province' href='javascript:void(0)'>" + data.province[i] + "</a>")
+    }
+    $(".dropdown-item.SenderProvince").click(function() {
+      $(this).parent().prev().text($(this).text())
+      $("#SenderCity").removeClass("disabled")
+      $.ajax('/getCity',{
+        dataType:'json',
+        data:{'province':$(this).text()}
+      }).done(function(data) {
+    var CityList = $("#CityList")
+		CityList.children().remove()
+		CityList.prev().text("选择城市")
+        for (var i = 0; i < data.city.length; i++) {
+          CityList.append("<a class='dropdown-item btn btn-primary city' href='javascript:void(0)'>" + data.city[i] + "</a>")
+        }
+        $(".dropdown-item.SenderCity").click(function() {
+          $(this).parent().prev().text($(this).text())
+        })
+      })
+    })
+  })
+	  $.ajax('/getProvince', {dataType:'json'}).done(function(data){
+    var ProvinceList = $("#ProvinceList")
+    for (var i = 0; i < data.province.length; i++) {
+      ProvinceList.append("<a class='dropdown-item btn btn-primary province' href='javascript:void(0)'>" + data.province[i] + "</a>")
+    }
+    $(".dropdown-item.ReceiverProvince").click(function() {
+      $(this).parent().prev().text($(this).text())
+      $("#ReceiverCity").removeClass("disabled")
+      $.ajax('/getCity',{
+        dataType:'json',
+        data:{'province':$(this).text()}
+      }).done(function(data) {
+    var CityList = $("#CityList")
+		CityList.children().remove()
+		CityList.prev().text("选择城市")
+        for (var i = 0; i < data.city.length; i++) {
+          CityList.append("<a class='dropdown-item btn btn-primary city' href='javascript:void(0)'>" + data.city[i] + "</a>")
+        }
+        $(".dropdown-item.ReceiverCity").click(function() {
+          $(this).parent().prev().text($(this).text())
+        })
+      })
+    })
+  })
 })
 
-function validateSignIn(){
+function validateform(){
 	$("#formUser").validate({
 		submitHandler:function(){
 			Func_userform();
-			Func_addr();
 		},
 		rules:{
 			SenderName:{
 				required:true
-				minlength: 100
+				maxlength: 100
 			},
 			SenderTele:{
 				required:true
-				minlength: 100
+				maxlength: 100
 			},
 			SenderRegion:{
 				required:true
-				minlength:100
+				maxlength:100
 			},
 			SenderStreet:{
 				required:true
-				minlength:100
+				maxlength:100
 			},
 			ReceiverName:{
 				required:true
-				minlength: 100
+				maxlength: 100
 			},
 			ReceiverTele:{
 				required:true
-				minlength: 100
+				maxlength: 100
 			},
 			Receiverregion:{
 				required:true
-				minlength: 100
+				maxength: 100
 			},
 			Receiverstreet:{
 				required:true
-				minlength: 100
+				maxlength: 100
 			},
 			
 		},
 		messages:{
 			SenderName:{
 				required:"请输入寄件人姓名",
-				minlength:"寄件人姓名不能超过100个字符"
+				maxlength:"寄件人姓名不能超过100个字符"
 			},
 			SenderTele:{
 				required:"请输入寄件人联系方式",
-				minlength:"寄件人联系方式不能超过100个字符"
+				maxlength:"寄件人联系方式不能超过100个字符"
 			},
 			SenderRegion:{
 				required:"请输入区/县",
-				minlength:"寄件人所在区/县不能超过100个字符"
+				maxlength:"寄件人所在区/县不能超过100个字符"
 			},
 			SenderStreet:{
 				required:"请输入街道地址",
-				minlength:"寄件人所在街道地址不能超过100个字符"
+				maxlength:"寄件人所在街道地址不能超过100个字符"
 			},
 			ReceiverName:{
 				required:"请输入寄件人姓名",
-				minlength:"收件人姓名不能超过100个字符"
+				maxlength:"收件人姓名不能超过100个字符"
 			},
 			ReceiverTele:{
 				required:"请输入寄件人联系方式",
-				minlength:"收件人联系方式不能超过100个字符"
+				maxlength:"收件人联系方式不能超过100个字符"
 			},
 			ReceiverRegion:{
 				required:"请输入区/县",
-				minlength:"收件人所在区/县不能超过100个字符"
+				maxlength:"收件人所在区/县不能超过100个字符"
 			},
 			ReceiverStreet:{
 				required:"请输入街道地址",
-				minlength:"收件人所在街道地址不能超过100个字符"
+				maxlength:"收件人所在街道地址不能超过100个字符"
 			},
 		}
 	})
 }
 
 function Func_userform(){
-	$.ajax("/userform/",{
+	$.ajax({
+		url:"/userform/",
 		dataType:'json',
 		type: 'POST',
 		data:{
@@ -99,22 +149,4 @@ function Func_userform(){
 		}
 	})
 }
-function Func_addr(){
-	$.ajax("/addr/",{
-		dataType:'json',
-		type: 'GET',
-		data:{
-			"SenderRegion":$("#SenderStreet").val(),
-			"SenderStreet":$("#SenderStret").val(),
-			"ReceiverRegion":$("#ReceiverStreet").val(),
-			"ReceiverStreet":$("#ReceiverStret").val()
-		}
-	}).done(function(data){
-		if(data.statCode != 0){
-			alert(data.errormessage)
-		}else{
-			alert(data.successmessage)
-			location.replace("/send_package/")
-		}
-	})
-}
+
