a2089e1de0fd6b606d7dc46a00e9049ee07eb31c
William Smith <weiliang84@yeah.net>
2017-12-24 02:54:31 +0800

    debug

--- Log_web/static/form.js
+++ Log_web/static/form.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
   //alert("!!!")
-	 validateform()
+	 validateForm()
 
-  $.ajax('/getProvince', {dataType:'json'}).done(function(data){
-    var ProvinceList = $("#ProvinceList")
+  $.ajax("/userform/", {dataType:'json'}).done(function(data){
+    var ProvinceList = $("#SenderProvinceList")
     for (var i = 0; i < data.province.length; i++) {
       ProvinceList.append("<a class='dropdown-item btn btn-primary province' href='javascript:void(0)'>" + data.province[i] + "</a>")
     }
     $(".dropdown-item.SenderProvince").click(function() {
       $(this).parent().prev().text($(this).text())
       $("#SenderCity").removeClass("disabled")
-      $.ajax('/getCity',{
+      $.ajax('/userform/',{
         dataType:'json',
         data:{'province':$(this).text()}
       }).done(function(data) {
-    var CityList = $("#CityList")
+    var CityList = $("#SenderCityList")
 		CityList.children().remove()
 		CityList.prev().text("选择城市")
         for (var i = 0; i < data.city.length; i++) {
@@ -26,19 +26,19 @@ $(document).ready(function() {
       })
     })
   })
-	  $.ajax('/getProvince', {dataType:'json'}).done(function(data){
-    var ProvinceList = $("#ProvinceList")
+	  $.ajax("/userform/", {dataType:'json'}).done(function(data){
+    var ProvinceList = $("#ReceiverProvinceList")
     for (var i = 0; i < data.province.length; i++) {
       ProvinceList.append("<a class='dropdown-item btn btn-primary province' href='javascript:void(0)'>" + data.province[i] + "</a>")
     }
     $(".dropdown-item.ReceiverProvince").click(function() {
       $(this).parent().prev().text($(this).text())
       $("#ReceiverCity").removeClass("disabled")
-      $.ajax('/getCity',{
+      $.ajax('/userform/',{
         dataType:'json',
         data:{'province':$(this).text()}
       }).done(function(data) {
-    var CityList = $("#CityList")
+    var CityList = $("#ReceiverCityList")
 		CityList.children().remove()
 		CityList.prev().text("选择城市")
         for (var i = 0; i < data.city.length; i++) {
@@ -52,47 +52,30 @@ $(document).ready(function() {
   })
 })
 
-function validateform(){
-	$("#formUser").validate({
+function validateForm(){
+	$("#SenderForm").validate({
 		submitHandler:function(){
-			Func_userform();
+			Func_Senderuserform();
 		},
 		rules:{
 			SenderName:{
-				required:true
+				required:true,
 				maxlength: 100
 			},
 			SenderTele:{
-				required:true
+				required:true,
 				maxlength: 100
 			},
 			SenderRegion:{
-				required:true
+				required:true,
 				maxlength:100
 			},
 			SenderStreet:{
-				required:true
+				required:true,
 				maxlength:100
 			},
-			ReceiverName:{
-				required:true
-				maxlength: 100
-			},
-			ReceiverTele:{
-				required:true
-				maxlength: 100
-			},
-			Receiverregion:{
-				required:true
-				maxength: 100
-			},
-			Receiverstreet:{
-				required:true
-				maxlength: 100
-			},
-			
 		},
-		messages:{
+		messages:{	
 			SenderName:{
 				required:"请输入寄件人姓名",
 				maxlength:"寄件人姓名不能超过100个字符"
@@ -108,7 +91,33 @@ function validateform(){
 			SenderStreet:{
 				required:"请输入街道地址",
 				maxlength:"寄件人所在街道地址不能超过100个字符"
+				},
+			}
+	})
+	$("#ReceiverForm").validate({
+		submitHandler:function(){
+			Func_Receiveruserform();
+		},
+		rules:{
+		ReceiverName:{
+				required:true,
+				maxlength: 100
 			},
+			ReceiverTele:{
+				required:true,
+				maxlength: 100
+			},
+			ReceiverRegion:{
+				required:true,
+				maxength: 100
+			},
+			ReceiverStreet:{
+				required:true,
+				maxlength: 100
+			},	
+		},
+		messages:{
+
 			ReceiverName:{
 				required:"请输入寄件人姓名",
 				maxlength:"收件人姓名不能超过100个字符"
@@ -125,20 +134,31 @@ function validateform(){
 				required:"请输入街道地址",
 				maxlength:"收件人所在街道地址不能超过100个字符"
 			},
-		}
+		},
+	})
+	$("#TransTypeForm").validate({
+		submitHandler:function(){
+			Func_Transtypeform();
+		},
+		rules:{
+		TransType:{
+				required:true,
+			},
+		},
 	})
 }
 
-function Func_userform(){
-	$.ajax({
-		url:"/userform/",
+function Func_Senderuserform(){
+	$.ajax("/userform/",{
 		dataType:'json',
 		type: 'POST',
 		data:{
 			"SenderName":$("#SenderName").val(),
 			"SenderTele":$("#SenderTele").val(),
-			"ReceiverName":$("#ReceiverName").val(),
-			"ReceiverTele":$("#ReceiverTele").val()
+			"SenderProvince":$("#SenderProvince").val(),
+			"SenderCity":$("#SenderCity").val(),
+			"SenderRegion":$("#SenderRegion").val(),
+			"SenderStreet":$("#SenderStreet").val(),			
 		}
 	}).done(function(data){
 		if(data.statCode != 0){
@@ -150,3 +170,43 @@ function Func_userform(){
 	})
 }
 
+function Func_Receiveruserform(){
+	$.ajax({
+		url:"/userform/",
+		dataType:'json',
+		type: 'POST',
+		data:{			
+			"ReceiverName":$("#ReceiverName").val(),
+			"ReceiverTele":$("#ReceiverTele").val(),
+			"ReceiverProvince":$("#ReceiverProvince").val(),
+			"ReceiverCity":$("#ReceiverCity").val(),
+			"ReceiverRegion":$("#ReceiverRegion").val(),
+			"ReceiverStreet":$("#ReceiverStreet").val(),
+		}
+	}).done(function(data){
+		if(data.statCode != 0){
+			alert(data.errormessage)
+		}else{
+			alert(data.successmessage)
+			location.replace("/send_package/")
+		}
+	})
+}
+
+function Func_Transtypeform(){
+	$.ajax({
+		url:"/userform/",
+		dataType:'json',
+		type: 'POST',
+		data:{
+			"TransType":$("TransType").val()
+		}
+	}).done(function(data){
+		if(data.statCode != 0){
+			alert(data.errormessage)
+		}else{
+			alert(data.successmessage)
+			location.replace("/send_package/")
+		}
+	})
+}
