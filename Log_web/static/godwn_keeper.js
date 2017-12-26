4bea2cecb1b660845284ccc0d17ef67ae500a337
William Smith <weiliang84@yeah.net>
2017-12-26 22:44:59 +0800

    godwn_keeper.js修改

--- Log_web/static/godwn_keeper.js
+++ Log_web/static/godwn_keeper.js
@@ -1,15 +1,13 @@
-  $(document).ready(function() {
+ $(document).ready(function() {
 	 validateForm(),
 	 $.ajax("/get_package_id/", {dataType:'json'}).done(function(data){
-    var package_id_list = $("#package_id_list")
-    for (var i = 0; i < data.package_id.length; i++) {
+     var package_id_list = $("#package_id_list")
+     for (var i = 0; i < data.package_id.length; i++) {
       package_id_list.append("<a class='dropdown-item btn btn-primary package_id' href='javascript:void(0)'>" + data.package_id[i] + "</a>")
     }
-    $(".dropdown-item.spackage_id").click(function() {
-      
-	  $(this).parent().prev().text($(this).text())
-		$("#SenderProvince").removeClass("disabled")	  
-		
+     $(".dropdown-item.spackage_id").click(function() {
+       $(this).parent().prev().text($(this).text())
+	   $("#SenderProvince").removeClass("disabled")	  
 		$.ajax("/getProvince/", {dataType:'json'}).done(function(data){
 			var ProvinceList = $("#SenderProvinceList")
 			for (var i = 0; i < data.province.length; i++) {
@@ -53,17 +51,17 @@
 				$(this).parent().prev().text($(this).text())
 					})
 				})
-	})
+  })
+ })
 })
-				})
 
 function validateForm(){
 	$("#keeper_operation_Form").validate({
 		submitHandler:function(){
 			Func_Operation_Form();
 		}
-		})
-		}
+	})
+}
 function Func_Operation_Form(){
 	$.ajax("/keeper_operation/",{
 		dataType:'json',
